d0f9e03a867849b77281caa14541e31727f956e5
ffalcone11802 <f.falcone3@studenti.poliba.it>
2024-09-04 15:05:44 +0200

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -9,23 +9,10 @@ const userSchema = new mongoose.Schema({
     lastName: {
         type: String
     },
-    username: {
-        type: String
-    },
-    password: {
-        type: String
-    },
     items: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: "Item"
     }]
-})
-
-// Password hashing
-userSchema.pre("save", function(next) {
-  const user = this
-  bcrypt.hash(user.password, 10)
-  .then(h => {user.password = h; next()})
 })
 
 module.exports = mongoose.model("User", userSchema)
